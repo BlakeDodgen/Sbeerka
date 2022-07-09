27661c39e6185ff67a563c8ef0de8d66cd372604
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-07-09 21:30:18 +0200

    seearch bar

--- resources/js/Index/SearchBar.js
+++ resources/js/Index/SearchBar.js
@@ -1,6 +1,15 @@
 function SearchBar() {
     return (
         <div className="search">
+            {/* create a new component ?? */}
+            <select>
+                <option>Breweries</option>
+                <option>Beer</option>
+                <option>Flavors</option>
+                <option>Alcohol Content</option>
+                <option>Beer Type</option>  
+                <option>Random Beer</option>               
+            </select>
             <input name="search" onChange={(event) => setSearchQuery(event.target.value)} value="" />
         </div>
     )
