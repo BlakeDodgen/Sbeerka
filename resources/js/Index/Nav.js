1c4ca4559c32a97aaf5a5bb1e3fe253a171693de
Blake Dodgen <blakedodgen@gmail.com>
2022-07-09 21:22:44 +0200

    styles linked

--- resources/js/Index/Nav.js
+++ resources/js/Index/Nav.js
@@ -5,11 +5,11 @@ function Nav() {
 
     return (
         <div className='nav'>
-            <Link to="/">LOGO</Link>
+            {/* <Link to="/">LOGO</Link>
             <SearchBar />
             <Link to="/">About</Link>
             <Link to="/">Log In</Link>
-            <Link to="/">Sign Up</Link>
+            <Link to="/">Sign Up</Link> */}
         </div>
     )
 }
