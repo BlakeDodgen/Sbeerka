98b5f9347306ba4722f33348dbc0be7150df4dc0
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-07-10 18:28:55 +0200

    select with react syntax

--- resources/js/Index/SearchBar.js
+++ resources/js/Index/SearchBar.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
+
 function SearchBar() {
+
+    // default value after render is set as Search All
+    const [search, setSearch] = useState("All");
+
+    //function changes state when option is selected
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value);
+        console.log(e.target.value);
+    }
+
+    
     return (
         <div className="search">
             {/* create a new component ?? */}
-            <select>
-            <option selected disabled>ALL</option>
-                <option>Breweries</option>
-                <option>Beer</option>
-                <option>Flavors</option>
-                <option>Alcohol Content</option>
-                <option>Beer Type</option>  
-                <option>Random Beer</option>               
+            <select value={search} onChange={handleSearchChange}>
+                <option value="all">ALL</option>
+                <option value="breweries">Breweries</option>
+                <option value="beer">Beer</option>
+                <option value="flavors">Flavors</option>
+                <option value="alcohol-content">Alcohol Content</option>
+                <option value="beer-type">Beer Type</option>  
+                <option value="random-beer">Random Beer</option>               
             </select>
             <input name="search" onChange={(event) => setSearchQuery(event.target.value)} value="" />
         </div>
