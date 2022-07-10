22e341a2b51492c34a65c0bfa25cf460f9e91f0a
Blake Dodgen <blakedodgen@gmail.com>
2022-07-10 20:52:44 +0200

    log in drop down functioning

--- resources/js/Index/Nav.js
+++ resources/js/Index/Nav.js
@@ -10,7 +10,19 @@ function Nav() {
             <SearchDropDown />
             <SearchBar />
             <a href="/homepage">About</a>
-            <div className='nav__log-in'><img className='nav__icon' src="./img/profile.svg" alt="profile" /><a href="/homepage">Log In</a></div>
+            <div className='dropdown'>
+                <button className='dropbtn'>
+                    <div className='nav__log-in'>
+                        <img className='nav__icon' src='./img/profile.svg' alt='profile' />
+                        <a href='/homepage'>Log In</a>
+                    </div>
+                </button>
+                <div className='dropdown-content'>
+                    <a href="#">Settings</a>
+                    <a href="#">Beer Collection</a>
+                    <a href="#">Log Out</a>
+                </div>
+            </div>
             <a href="/homepage">Sign Up</a>
         </div>
     )
