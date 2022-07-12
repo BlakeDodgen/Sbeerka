b14e71fe8f79498e81e3acdb73f6efe38e89dc8e
BenHbrt <benherbert1983@googlemail.com>
2022-07-12 11:37:20 +0200

    Search APIs

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -8,49 +8,82 @@ use App\Models\Brewery;
 use App\Models\Beer;
 use App\Models\BeerType;
 use App\Models\User;
+use App\Models\Review;
 
 class SearchController extends Controller
 {
     public function search(Request $request)
     {
+        // Assigning the two search variables from the request object
         $searchType = $request->search;
         $searchText = $request->text;
 
+        // Different searches
+
+
         if ($searchType == "beername") {
             $beers = Beer::where('name', 'like', ('%'.$searchText.'%'))
-                    ->with('beerType', 'brewery', 'beerPic')
+                    // ->with('beerType', 'brewery', 'beerPic')
+                    ->with([
+                        'beerType',
+                        'beerPic',
+                        'brewery',
+                        'brewery.breweryPic',
+                        'brewery.user',
+                        'reviews'
+                    ])
                     ->get();
             return $beers;
+
         } elseif ($searchType == "breweries") {
-            $users = User::select('id')
+            $users = User::select('id', 'brewery_name')
                     ->where('brewery_name', 'like', ('%'.$searchText.'%'))
+                    ->with([
+                        'brewery',
+                        'brewery.breweryPic',
+                        'brewery.beers',
+                        'brewery.beers.beerPic',
+                        'brewery.beers.reviews'
+                    ])
                     ->get();
-            $userslist = [];
-            foreach($users as $user) {
-                array_push($userslist, $user->id);
-            }
-            dd($userslist);
-            // $breweries = Brewery::where('id', 'in', $userslist)
-            //         ->with('brewery')
-            //         ->with ('beers')
-            //         ->get();
-            return $users;
+            return $users;  
+
         } elseif ($searchType == "beer-type") {
             $beerType = BeerType::where('type', 'like', ('%'.$searchText.'%'))
-                    ->with('beers')
+                    ->with([
+                        'beers',
+                        'beers.beerPic',
+                        'beers.reviews'
+                    ])
                     ->get();
             return $beerType;
+
+        } elseif ($searchType == "city") {
+            $breweries = Brewery::where('city', 'like', ('%'.$searchText.'%'))
+                    ->with([
+                        'breweryPic',
+                        'user',
+                        'beers',
+                        'beers.beerPic',
+                        'beers.reviews'
+                    ])
+                    ->get();
+            return $breweries;
+
+        } elseif ($searchType == "country") {
+            $breweries = Brewery::where('country', 'like', ('%'.$searchText.'%'))
+                    ->with([
+                        'breweryPic',
+                        'user',
+                        'beers',
+                        'beers.beerPic',
+                        'beers.reviews'
+                    ])
+                    ->get();
+            return $breweries;
+
         } else {
             return 'Hello! This still needs some work!';
         }
     }
 }
-
-// all
-// breweries -DONE
-// beername -DONE
-// city
-// flavors
-// alcohol-content
-// beer-type -DONE
-// random-beer
