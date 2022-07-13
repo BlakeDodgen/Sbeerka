c19f5d34d0caf8032a4498bdcd49aac70c75aa07
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-07-13 12:35:45 +0200

    test

--- resources/js/Index/auth/Login.js
+++ resources/js/Index/auth/Login.js
@@ -1,11 +1,30 @@
-
+import { useState } from "react";
 
 function Login() {
+    const [values, setValues] = useState();
 
+    const handleSignUp = async (e)=>{
+        e.preventDefault();
 
-    return (
-        <p>HI Iam Login Page</p>
-    )
+        //  const response = await axios.post('/register', values);
+        //  const response_data = response.data;
+    }
+
+    const handleChange = (e) => {
+        setValues(previous_values => {
+            return ({
+                ...previous_values,
+                [e.target.name]: e.target.value
+            });
+        });
+        return (<>
+            <p>Login</p>
+            <label>user name </label>
+            <input type="text" name="username" value={values.username} onChange={handleChange} />
+            <label>Password </label>
+            <input type="password" name="password" value={values.password} onChange={handleChange} />
+        </>
+        )
+    }
 }
-
-export default Login;
+    export default Login;
