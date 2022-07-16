4bff3161225863e0747e3221edcc3f3023d31f4b
Blake Dodgen <blakedodgen@gmail.com>
2022-07-16 23:57:06 +0200

    few more fixes with forms jsx and some dropshadows

--- resources/js/Index/auth/Login.js
+++ resources/js/Index/auth/Login.js
@@ -48,7 +48,7 @@ function Login() {
         });
     };
 
-   
+
 
     return (
         <>
@@ -58,7 +58,7 @@ function Login() {
                 <div className="form__container">
                     <label>User name </label>
                     <br />
-                    <input
+                    <input className="form__input"
                         type="text"
                         name="email"
                         value={values.email}
@@ -68,7 +68,7 @@ function Login() {
                 <div className="form__container">
                     <label>Password </label>
                     <br />
-                    <input
+                    <input className="form__input"
                         type="password"
                         name="password"
                         value={values.password}
@@ -79,6 +79,7 @@ function Login() {
                 <button className="form__button">Log In</button>
                 {!!errorResponse && <span className="form__failed">Login Failed</span>}
                 <br />
+
             </form>
             {/* <button onClick={async () => {
             const user = await loadUser()
