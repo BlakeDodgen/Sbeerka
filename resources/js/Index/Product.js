fca1fc9d897b7dda61f8fb568712f7c8a113b33c
Blake Dodgen <blakedodgen@gmail.com>
2022-07-17 16:03:09 +0200

    media queries for responsive design added for forms handling and main page

--- resources/js/Index/Product.js
+++ resources/js/Index/Product.js
@@ -1,29 +1,31 @@
 import { Link } from "react-router-dom";
 
 const Product = (props) => {
-    
+
     //add the rating
     let ratingScore = 0;
-        props.data.reviews.forEach(review => {
-                ratingScore += review.rating;
-        })
-    let avRating = ratingScore/props.data.reviews.length
+    props.data.reviews.forEach(review => {
+        ratingScore += review.rating;
+    })
+    let avRating = ratingScore / props.data.reviews.length
     if (isNaN(avRating)) {
         avRating = 0;
     }
-    
 
     return (
         <>
-
             <div className="product-list__item">
+
+                <Link to={`/beers/${props.data.id}`}>
+                    <p className="product-list__name">{props.data.name}</p>
+                </Link>
                 <div className="product-list__image-container">
-                    <Link to={`/beers/${props.data.id}`}><img src={`/img/beers/${props.data.beer_pic.picture}`} className="product-list__img" alt="beer-item-img" /></Link>                   
+                    <Link to={`/beers/${props.data.id}`}><img src={`/img/beers/${props.data.beer_pic.picture}`} className="product-list__img" alt="beer-item-img" /></Link>
                 </div>
-                <Link to={`/beers/${props.data.id}`}><p className="product-list__name">{props.data.name}</p></Link>
-                <p className="product-list__rating">Rating: {avRating.toFixed(1)}</p>
+                <div className="product-list__rating-container">
 
-
+                    <p className="product-list__rating">Rating: {avRating.toFixed(1)}</p>
+                </div>
             </div>
         </>
     )
